d18c09615158d105995057d16ff7b3da70c104ee
Jose Seie <joseeugenioseie@hotmail.com>
2019-11-12 01:27:20 +0200

    App state, offline or online detection

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,34 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page padding class="flex flex-center">
+    <!-- @detected-condition fires when the connectivity status of the device changes -->
+    <offline @detected-condition="handleConnectivityChange">
+      <!-- Only renders when the device is online -->
+      <div slot="online">
+        <p>It looks like you're online! Here's all the things you can do...</p>
+        ...
+      </div>
+      <!-- Only renders when the device is offline -->
+      <div slot="offline">
+        <h6>You appear to be offline, that's okay!</h6>
+        ...
+      </div>
+    </offline>
   </q-page>
 </template>
 
 <script>
-export default {
-  name: 'PageIndex'
-}
+    import offline from 'v-offline';
+
+    export default {
+        name: 'PageIndex',
+        components: {
+            offline
+        },
+
+        methods: {
+            handleConnectivityChange(status) {
+                console.log(status);
+            }
+        }
+    }
 </script>
