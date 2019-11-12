4f476549bd8ee7fffd7914b7aeb72e964846b8fb
Nelzio <nelziositoe@gmail.com>
2019-11-12 16:29:21 +0200

    Vibrate

--- src/pages/account/Login.vue
+++ src/pages/account/Login.vue
@@ -1,69 +1,82 @@
 <template>
   <q-page>
     <!-- content -->
-    <div class="row q-pa-md login justify-center">
-      <div class="row justify-center">
-      <q-icon color="blue-9" 
-        size="100px" name="account_circle"
-      />
-      </div>
-      <div class="col-12">
-        Entrar ou se Inscrever
-      </div>
-      <div class="bg-grey-3 q-pa-lg shadow-10 col-md-4 col-12">
-        <q-form
-          ref="loginForm"
-          @submit="onSubmit"
-          @reset="onReset"
-          class="q-gutter-md"
-        >
-      <q-input
-        standout="bg-grey"
-        color="blue-9"
-        dense
-        v-model="user"
-        label="Nome do usuario"
-        lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
-      />
-
-      <q-input
-        standout="bg-grey"
-        color="blue-9"
-        @keyup.enter="login_account"
-        dense
-        placeholder="password"
-        v-model="password"
-        :type="isPwd ? 'password' : 'text'"
-        lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type pass']"
+    <div>
+      <q-tab-panels
+        v-model="tab"
+        animated
+        swipeable
+        transition-prev="scale"
+        transition-next="scale"
+        class="text-center"
       >
-        <template v-slot:append>
-          <q-icon
-            :name="isPwd ? 'visibility_off' : 'visibility'"
-            class="cursor-pointer"
-            @click="isPwd = !isPwd"
-          />
-        </template>
-      </q-input>
+        <q-tab-panel name="login">
+          <div class="row login justify-center q-gutter-y-lg">
+            <q-icon color="blue-9" 
+              size="100px" name="account_circle"
+            />
+            <!-- <div class="col-12">
+              Entrar ou se Inscrever
+            </div> -->
+            <div class="bg-grey-3 q-pa-lg shadow-10 col-md-4 col-12">
+              <q-form
+                ref="loginForm"
+                @submit="onSubmit"
+                @reset="onReset"
+                class="q-gutter-md"
+              >
+                <q-input
+                  standout="bg-grey"
+                  color="blue-9"
+                  dense
+                  v-model="user"
+                  label="Nome do usuario"
+                  lazy-rules
+                  :rules="[ val => val && val.length > 0 || 'Please type something']"
+                />
 
-      <div>
-        <q-btn label="login" type="login" color="primary" class="full-width"/>
-      </div>
+                <q-input
+                  standout="bg-grey"
+                  color="blue-9"
+                  @keyup.enter="login_account"
+                  dense
+                  placeholder="password"
+                  v-model="password"
+                  :type="isPwd ? 'password' : 'text'"
+                  lazy-rules
+                  :rules="[ val => val && val.length > 0 || 'Please type pass']"
+                >
+                  <template v-slot:append>
+                    <q-icon
+                      :name="isPwd ? 'visibility_off' : 'visibility'"
+                      class="cursor-pointer"
+                      @click="isPwd = !isPwd"
+                    />
+                  </template>
+                </q-input>
+                <div>
+                  <q-btn label="login" type="login" color="primary" class="full-width"/>
+                </div>
 
-      <q-item>
-        <q-item-section>
-          <q-checkbox v-model="teal" label="Remenber me" />
-        </q-item-section>
-        <q-item-section>
-          <router-link to="/">
-            Recuperar senha
-          </router-link>
-        </q-item-section>
-      </q-item>
-    </q-form>
+                <q-item>
+                  <q-item-section>
+                    <q-checkbox v-model="teal" label="Remenber me" />
+                  </q-item-section>
+                  <q-item-section>
+                    <router-link to="/">
+                      Recuperar senha
+                    </router-link>
+                  </q-item-section>
+                </q-item>
+              </q-form>
+            </div>
+          </div>
+        </q-tab-panel>
 
-    </div>
+        <q-tab-panel name="create">
+          <div class="text-h6">Create account</div>
+        </q-tab-panel>
+      </q-tab-panels>
     </div>
     
   </q-page>
@@ -78,7 +91,8 @@ export default {
       user: '',
       password: '',
       isPwd: true,
-      dialogLogin: false
+      dialogLogin: false,
+      tab: 'login'
     }
   },
   components: {
