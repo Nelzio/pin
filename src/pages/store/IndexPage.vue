9be7335a7bc705365548e2b0bfc74176cc06b436
Jose Seie <joseeugenioseie@hotmail.com>
2019-11-13 00:16:43 +0200

    Melhoria de dark mode na exposição

--- src/pages/store/IndexPage.vue
+++ src/pages/store/IndexPage.vue
@@ -7,15 +7,28 @@
     <!-- content -->
     <div class="row">
       <div class="col-6 q-pa-sm" v-for="i in 16" :key="i">
-        <q-card class="my-card">
+        <q-card class="my-card" :class="[appMode.textColorOptional, appMode.bgColor]">
           <q-img src="https://cdn.awsli.com.br/600x1000/60/60876/produto/28035638/9e1cebfb32.jpg" spinner-color="white" />
           <q-card-section>
             <div class="text-body1">Armando Jose</div>
             <div class="text-subtitle2">Sandalias de cabdal</div>
           </q-card-section>
           <q-card-actions align="right">
-            <q-btn size="xs" outline rounded color="white" text-color="black" label="Detalhes" to="/store/details" />
-            <q-btn size="xs" round outline  icon="volume_up"/>
+            <q-btn
+               size="xs"
+               outline
+               rounded
+               :text-color="appMode.modeName === 'dark' ? 'white' : 'primary'"
+               label="Detalhes"
+               to="/store/details"
+            />
+            <q-btn
+               size="xs"
+               round
+               :color="appMode.modeName === 'dark' ? 'white' : 'primary'"
+               outline
+               icon="volume_up"
+            />
           </q-card-actions> 
         </q-card>
       </div>
@@ -44,7 +57,7 @@
     },
       computed: {
           ...mapState('settings', [
-              'settings'
+              'settings', 'appMode'
           ]),
       },
     methods: {
